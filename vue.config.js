6c132dd636ae65b6d7eda7f6f9c8c99746ec1408
Aulon Mujaj <aulon.mujaj@gmail.com>
2018-12-23 12:06:54 +0100

    moved to docs folder

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   lintOnSave: undefined,
+  baseUrl: undefined,
+  outputDir: 'docs',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined,
 };
