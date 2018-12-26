76fd4cf996e15d7da04504f855a1291c29029434
Aulon Mujaj <aulon.mujaj@gmail.com>
2018-12-26 17:57:14 +0100

    Small frontpage information

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,8 @@
   <VContainer
     fluid
   >
-    <h1>Add information here</h1>
+    <h1>Welcome to your top listening tracks and artists on Spotify</h1>
+    <span>On the top right corner you can find links to the artists page or tracks page.</span>
   </VContainer>
 </template>
 
