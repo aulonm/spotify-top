4ef84f03b3defcd0d48cf050d353d481d8c2b1ca
Aulon Mujaj <aulon.mujaj@gmail.com>
2019-07-14 17:02:51 +0200

    updaed dependencies and build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   lintOnSave: undefined,
-  baseUrl: '/spotify-top/',
+  publicPath: '/spotify-top/',
   outputDir: 'docs',
   assetsDir: undefined,
   runtimeCompiler: undefined,
